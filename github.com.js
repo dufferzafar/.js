1adf3c76cdfb8e3003324d3bc6cd5588d5aba80f
Shadab Zafar <dufferzafar0@gmail.com>
2014-11-29 02:57:25 +0530

    Removed a few things from the usual Github page

---
+++ github.com.js
@@ -0,0 +1,10 @@
+// Remove the Github logo on top-right corner
+$('a[class^=header-logo-]').remove();
+
+// Hide the clone urls.
+$('div[data-protocol-type^=subversion]').remove();
+$('p.clone-options').remove();
+$('div.clone-url').remove();
+
+// Remove the Clone on Windows button.
+$('a.minibutton.sidebar-button:first').remove();
